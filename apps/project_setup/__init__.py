48b173f9e541e4a66995012837032536c12f2aa8
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-08-30 02:47:08 -0400

    Disabled cog icon for menu uniformity

--- apps/project_setup/__init__.py
+++ apps/project_setup/__init__.py
@@ -3,4 +3,5 @@ from django.utils.translation import ugettext_lazy as _
 from navigation.api import register_top_menu
 
 #TODO: FIXME dynamic children_path_regext on api register_setup
-register_top_menu('setup_menu', link={'text': _(u'setup'), 'view': 'setup_list', 'famfam': 'cog'}, children_path_regex=[r'^settings/', r'^user_management/', r'^permissions'], position=-2)
+#register_top_menu('setup_menu', link={'text': _(u'setup'), 'view': 'setup_list', 'famfam': 'cog'}, children_path_regex=[r'^settings/', r'^user_management/', r'^permissions'], position=-2)
+register_top_menu('setup_menu', link={'text': _(u'setup'), 'view': 'setup_list'}, children_path_regex=[r'^settings/', r'^user_management/', r'^permissions'], position=-2)
