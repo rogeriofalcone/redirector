fcad19c9e53a9ad9445e4dcd030834044c5c6351
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-09-16 00:53:30 -0400

    Added Link class model to the mechanic app, this is the first step to deploy class based intercepted links for the upcomming CMS

--- apps/mechanic/urls.py
+++ apps/mechanic/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('mechanic.views',
     url(r'^simple/site/(?P<site_domain>[a-zA-Z0-9._]+)/url/(?P<url>.*)$', 'fetch', (), 'fetch'),
     url(r'^simple/site/(?P<site_domain>[a-zA-Z0-9._]+)/$', 'fetch', (), 'fetch'),
-    url(r'^simple/(?P<url>.*)$', 'fetch', (), 'fetch'),
+    url(r'^simple/url/(?P<url>.*)$', 'fetch', (), 'fetch'),
     url(r'^simple/$', 'fetch', (), 'fetch'),
     
     url(r'^coded/site/(?P<site_domain>[a-zA-Z0-9._]+)/url/(?P<coded_url>.*)$', 'fetch_coded', (), 'fetch_coded'),
     url(r'^coded/site/(?P<site_domain>[a-zA-Z0-9._]+)/$', 'fetch_coded', (), 'fetch_coded'),
-    url(r'^coded/(?P<coded_url>.*)$', 'fetch_coded', (), 'fetch_coded'),
+    url(r'^coded/url/(?P<coded_url>.*)$', 'fetch_coded', (), 'fetch_coded'),
     url(r'^coded/$', 'fetch_coded', (), 'fetch_coded'),
 )
