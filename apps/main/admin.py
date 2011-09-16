5036da5900f526ac3d6284c98404ec63d68286f4
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-09-16 04:09:02 -0400

    Added URL wrapper class, added Tree model class

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
-from main.models import CurrentSite
+from mptt.admin import MPTTModelAdmin
+
+from main.models import CurrentSite, URL, Tree
    
 
 class CurrentSiteAdmin(admin.ModelAdmin):
@@ -10,4 +12,21 @@ class CurrentSiteAdmin(admin.ModelAdmin):
     list_display_links = ('pk',)
     list_editable = ('site', 'selected',)
     
+    
+class URLAdmin(admin.ModelAdmin):
+    model = URL
+    list_display = ('title', 'url', 'enabled')
+    #list_display_links = ('pk',)
+    #list_editable = ('site', 'selected',)
+    
+    
+class TreeAdmin(MPTTModelAdmin):
+    model = Tree
+    list_display = ('pk', 'title', 'slug', 'order', 'content_type', 'object_id', 'enabled')
+    list_display_links = ('pk',)
+    list_editable = ('title', 'slug', 'order', 'enabled')
+
+
 admin.site.register(CurrentSite, CurrentSiteAdmin)
+admin.site.register(URL, URLAdmin)
+admin.site.register(Tree, TreeAdmin)
