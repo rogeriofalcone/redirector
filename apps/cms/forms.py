838e12a64178f50d1580201c5bfa9982b8de257a
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-10-05 08:31:34 -0400

    Reduce description field size to improve usable area

--- apps/cms/forms.py
+++ apps/cms/forms.py
@@ -1,18 +1,18 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 
 from markitup.widgets import MarkItUpWidget
+#from markitup import settings as markitup_settings
 
 from cms.models import Page
 
 
 class PageForm_create(forms.ModelForm):
-    #content = forms.CharField(widget=MarkItUpWidget(
-    #    attrs={'cols': 80, 'rows': 20},
-    #    markitup_set='markitup/sets/wiki'
-    #    )
-    #)
-   
+    def __init__(self, *args, **kwargs):
+        super(PageForm_create, self).__init__(*args, **kwargs)
+        self.fields['description'].widget.attrs={'cols': 80, 'rows': 2}
+               
     class Meta:
         model = Page
         exclude = ('content',)
@@ -20,18 +20,14 @@ class PageForm_create(forms.ModelForm):
 
         
 class PageForm_edit(forms.ModelForm):
-    #content = forms.CharField(widget=MarkItUpWidget(
-    #    attrs={'cols': 80, 'rows': 20},
-    #    markitup_set='markitup/sets/wiki'
-    #    )
-    #)
     def __init__(self, *args, **kwargs):
         super(PageForm_edit, self).__init__(*args, **kwargs)
         self.fields['content'].widget= MarkItUpWidget(
             attrs={'cols': 80, 'rows': 20},
-            markitup_set='markitup/sets/creole'
+            markitup_set='markitup/sets/%s' % self.instance.markup
         )
-   
+        self.fields['description'].widget.attrs={'cols': 80, 'rows': 2}
+
     class Meta:
         model = Page
         exclude = ('markup',)
