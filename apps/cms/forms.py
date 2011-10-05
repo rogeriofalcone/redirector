517fcf405ce70cdef2594c889cfc3bff4c35a718
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-10-05 06:37:25 -0400

    Added two step CMS page creation to support multiple makrups languages

--- apps/cms/forms.py
+++ apps/cms/forms.py
@@ -5,14 +5,34 @@ from markitup.widgets import MarkItUpWidget
 
 from cms.models import Page
 
-        
-class PageForm(forms.ModelForm):
-    content = forms.CharField(widget=MarkItUpWidget(
-        attrs={'cols': 80, 'rows': 20},
-        markitup_set='markitup/sets/wiki'
-        )
-    )
+
+class PageForm_create(forms.ModelForm):
+    #content = forms.CharField(widget=MarkItUpWidget(
+    #    attrs={'cols': 80, 'rows': 20},
+    #    markitup_set='markitup/sets/wiki'
+    #    )
+    #)
    
     class Meta:
         model = Page
+        exclude = ('content',)
+
+
+        
+class PageForm_edit(forms.ModelForm):
+    #content = forms.CharField(widget=MarkItUpWidget(
+    #    attrs={'cols': 80, 'rows': 20},
+    #    markitup_set='markitup/sets/wiki'
+    #    )
+    #)
+    def __init__(self, *args, **kwargs):
+        super(PageForm_edit, self).__init__(*args, **kwargs)
+        self.fields['content'].widget= MarkItUpWidget(
+            attrs={'cols': 80, 'rows': 20},
+            markitup_set='markitup/sets/creole'
+        )
+   
+    class Meta:
+        model = Page
+        exclude = ('markup',)
 
