ce3b181f4bc2c8874881747be640c0703754f4f5
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-10-15 05:13:27 -0400

    Add dynamic permissions support the tools top menu entry registration

--- apps/project_tools/api.py
+++ apps/project_tools/api.py
@@ -1,5 +1,9 @@
+from project_tools import tools_menu_entry
+
 tool_items = []
 
 
 def register_tool(link):
     tool_items.append(link)
+    tools_menu_entry['link'].setdefault('permissions', [])
+    tools_menu_entry['link']['permissions'].extend(link.get('permissions', []))
