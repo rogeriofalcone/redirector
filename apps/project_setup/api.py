28bced01ce817eee5a741a6a947508c711b978ea
Roberto Rosario <Roberto.Rosario.Gonzalez@gmail.com>
2011-10-15 05:12:56 -0400

    Add dynamic permissions support the setup top menu entry registration

--- apps/project_setup/api.py
+++ apps/project_setup/api.py
@@ -1,5 +1,9 @@
+from project_setup import setup_menu_entry
+
 setup_items = []
 
 
 def register_setup(link):
     setup_items.append(link)
+    setup_menu_entry['link'].setdefault('permissions', [])
+    setup_menu_entry['link']['permissions'].extend(link.get('permissions', []))
